065b1cc13352c1499519bb1261cbb7c8603e6dc9
Luca Spurgin <underscoreqazx@gmail.com>
2026-02-04 11:36:51 +1100

    create mathlesson.py

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # pro-lessons
-.
+
+This is my repository for all in class working on my college class
---
+++ mathlesson.py
@@ -0,0 +1,13 @@
+Num1 =int(input("Num? "))
+Num2 =int(input("Num? "))
+Num3 =int(input("Num? "))
+Num4 =int(input("Num? "))
+
+Select = input("which Number? ")
+Select2 =  input("")
+def doubleAddOne(x):
+    print(2 * x + 1)
+
+def tenPercentOff(price):
+    print (price * 0.9)
+
