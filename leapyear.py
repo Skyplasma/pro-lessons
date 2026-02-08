8af994d85fb633967682799cc3dd450b1af7ee85
Skyplasma <167046147+Skyplasma@users.noreply.github.com>
2026-02-09 10:42:26 +1100

    Create leapyear.py

---
+++ leapyear.py
@@ -0,0 +1,8 @@
+year = int(input("year? "))
+
+if year % 4 == 0:
+    if year % 100 == 0 and year % 400 != 0:
+        print(f"{year} is not a leap year!")
+    else: 
+        print(f"{year} is a leap year!")
+    
