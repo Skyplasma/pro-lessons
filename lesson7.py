6e7d3f911fcc05d3dd2e2670593ba4286ca686d5
Luca Spurgin <underscoreqazx@gmail.com>
2026-02-17 15:56:15 +1100

    dejia

--- lesson7.py
+++ lesson7.py
@@ -1,6 +1,6 @@
 a = [1,2,3,4,5]
 b = ['A','b','c','d','e']
-c = "hello"
+c = "hello pro gamers I SALUTE YOU HELLDIVER"
 
 print(c)
 print(c[0])
@@ -8,3 +8,25 @@ print(c[-1])
 print(c[4])
 print(c[len(c) - 1])
 
+print(list(c))
+print(f'length of a {len(a)}')
+
+for element in a:
+    print(element)
+
+for claracter in list(c):
+    print(claracter)
+
+vowels = list("aeiou")
+
+def countvow(sentence):
+    numvowels = 0
+    for charazard in list(sentence.lower()):
+        if charazard in vowels:
+            numvowels += 1
+    return  numvowels
+
+print(f"{c} has {countvow(c)} vowels")
+
+empty = []
+print(empty)
