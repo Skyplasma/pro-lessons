8856f761868186913dc9fd01c9ce25397b49313a
Luca Spurgin <underscoreqazx@gmail.com>
2026-03-18 12:12:35 +1100

    njbioaedadfnjifbade

--- Py exercises/lesson21-flask.py
+++ Py exercises/lesson21-flask.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello,world'
+    return ('Hello,world'
+        'asdf')
+
+
+@app.route('/physics')
+def broski():
+    return 'hi geogre'
+
+@app.route('/ping')
+def ping():
+        return jsonify({"ok": True})
+
+@app.route('/test')
+def serve_html():
+    return render_template("temp1.html")
 
 if __name__== "__main__":
     app.run(debug=True)
